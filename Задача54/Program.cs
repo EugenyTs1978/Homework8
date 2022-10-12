a46a586fcd3fb2c081b0074f0712818e2dc7a225
Eugeny1978 <eugenytsypyshev@rambler.ru>
2022-10-12 18:52:26 +0500

    Домашнее задание8

---
+++ Задача54/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+*/
+ int[,] generate2DArray(int height,int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height,width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i,j] = new Random().Next(randomStart, randomEnd+1 );
+        }
+    }
+    return twoDArray;
+}
+void SortStringArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1)-1; j++)
+        {
+          for (int k = 0; k < array.GetLength(1)-1; k++)
+          {
+            if (array[i,k] < array[i,k+1])
+            {
+                int temp = array[i,k];
+                array[i,k] = array[i,k + 1];
+                array[i,k + 1] = temp;
+            }
+          }  
+            
+        }
+    }
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i+"\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i+"\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] generatedArray = generate2DArray(5,7,-100,100);
+print2DArray(generatedArray);
+Console.WriteLine();
+SortStringArray(generatedArray);
+print2DArray(generatedArray);
+
